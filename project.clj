6628da005de7d57d8155f32029673b6fdee86d95
João Paulo Soares <jpsoares106@gmail.com>
2020-03-31 11:21:41 -0300

    2020-03-31

---
+++ project.clj
@@ -0,0 +1,49 @@
+(defproject react-med.core "0.1.0-SNAPSHOT"
+  :description "Web App para gerenciamento de receitas de pacientes médicos."
+  ;; :url "http://jponline.github.io/site"
+
+  :min-lein-version "2.9.1"
+
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [org.clojure/clojurescript "1.10.597"]
+
+                 ;; Has calendar dates, without times and timezones.
+                 [tick "0.4.23-alpha"]
+
+                 [re-frame "0.12.0"]
+                 [reagent "0.10.0"]
+
+                 ;; Should be production only
+                 ;; [day8.re-frame/tracing-stubs "0.5.3"]
+                 ]
+
+  :source-paths ["src"]
+
+  :aliases {"fig"       ["trampoline" "run" "-m" "figwheel.main"]
+            "fig:cards" ["trampoline" "run" "-m" "figwheel.main" "-b" "cards" "-r"]
+            "fig:cards-togithub"   ["run" "-m" "figwheel.main" "-O" "none" "-bo" "cards"]
+            "fig:dev" ["trampoline" "run" "-m" "figwheel.main" "-b" "dev" "-r"]
+            "fig:none-prod" ["run" "-m" "figwheel.main" "-O" "none" "-bo" "prod"]
+            "fig:prod" ["run" "-m" "figwheel.main" "-O" "advanced" "-bo" "prod"]}
+
+  :profiles {:dev
+             {:resource-paths ["target"]
+              :clean-targets ^{:protect false} ["target"]
+              :dependencies [[com.bhauman/figwheel-main "0.2.3"]
+                             [com.bhauman/rebel-readline-cljs "0.1.4"]
+                             [devcards "0.2.6"]
+                             [day8.re-frame/re-frame-10x "0.6.0"]
+                             [day8.re-frame/tracing "0.5.3"]
+                             [org.clojure/test.check "0.10.0"]
+                             ]
+              :repl-options  {:port 12344}}
+             :cards
+             {:resource-paths ["target"]
+              :clean-targets ^{:protect false} ["target"]
+              :dependencies [[com.bhauman/figwheel-main "0.1.9"]
+                             [com.bhauman/rebel-readline-cljs "0.1.4"]
+                             [devcards "0.2.6"]
+                             [day8.re-frame/re-frame-10x "0.6.0"]
+                             [day8.re-frame/tracing "0.5.3"]
+                             [org.clojure/test.check "0.10.0"]
+                             ]}})
---
+++ src/nefroapp/overview_cards.cljs
@@ -0,0 +1,106 @@
+(ns nefroapp.overview-cards
+  (:require
+    [devcards.core :as devcards :refer-macros [defcard deftest defcard-rg]]
+    ))
+
+;; Draw the devcards ui.
+(devcards.core/start-devcard-ui!)
+
+(defcard
+  (str "# Primeiro Card")
+  {}
+  {:frame false
+   :heading false})
+
+;; (defonce devcards-hidden (reagent/atom []))
+;;
+;; (defcard-rg hidding-cards
+;;   (let [card-container-style #js {:display "flex"
+;;                                   :justifyContent "space-evenly"
+;;                                   :padding "20px 0"}]
+;;     (fn [devcard-data _]
+;;       [:div.card-container
+;;        {:style card-container-style}
+;;        [:> material-button
+;;         {:id "hide-all"
+;;          :variant "outlined"
+;;          :size "small"
+;;          :color "secondary"
+;;          :onClick #(doseq [hidden-atom @devcards-hidden]
+;;                      (reset! hidden-atom true))}
+;;         "hide all"]
+;;        [:> material-button
+;;         {:id "show-all"
+;;          :variant "outlined"
+;;          :size "small"
+;;          :color "secondary"
+;;          :onClick #(doseq [hidden-atom @devcards-hidden]
+;;                      (reset! hidden-atom false))}
+;;         "show all"]]))
+;;   {}
+;;   {:frame false})
+
+;; (defcard-rg mobile-view
+;;   (fn [devcard-data _]
+;;     (let [{:keys [hidden?]} @devcard-data]
+;;       (swap! devcards-hidden conj hidden?)
+;;       [:<>
+;;        [:div.card-expander
+;;         {:onClick #(swap! hidden? not)
+;;          :style #js {:textAlign "center"}}
+;;         (if @hidden?
+;;           [:> material-icons-ExpandMore]
+;;           [:> material-icons-ExpandLess])]
+;;        [:div.card-container
+;;         {:style #js {:display "flex"
+;;                      ;; :justifyContent "space-evenly"
+;;                      :paddingBottom 20}}
+;;         [shell/error-boundary
+;;          {:if-error [shell/error-view]}
+;;          [:div.component-container
+;;           {:hidden @hidden?
+;;            :style #js {:width 360 :height 640
+;;                        :border "1px solid #00000038"}}
+;;           [routes/selected-view]]]]]))
+;;   {:hidden? (reagent/atom false)})
+;;
+;; (defcard-rg desktop-view
+;;   (fn [devcard-data _]
+;;     (let [{:keys [hidden?]} @devcard-data]
+;;       (swap! devcards-hidden conj hidden?)
+;;       [:<>
+;;        [:div
+;;         {:style #js {:display "flex"
+;;                      :justifyContent "space-evenly"
+;;                      :padding "15px 0"}}
+;;         [:div
+;;          [:div
+;;           {:class "com-rigsomelight-devcards-panel-heading com-rigsomelight-devcards-typog"
+;;            :style #js {:width "90vw"}}
+;;           [:a
+;;            {:href "#"
+;;             :style #js {:color "#666"}}
+;;            "desktop-view"]]]]
+;;        [:div.card-expander
+;;         {:onClick #(swap! hidden? not)
+;;          :style #js {:textAlign "center"}}
+;;         (if @hidden?
+;;           [:> material-icons-ExpandMore]
+;;           [:> material-icons-ExpandLess])]
+;;        [:div
+;;         {:style #js {:display "flex"
+;;                      :justifyContent "space-evenly"}}
+;;         [:div
+;;          [:div.card-container
+;;           {:style #js {:width "96vw"}}
+;;           [shell/error-boundary
+;;            {:if-error [shell/error-view]}
+;;            [:div.component-container
+;;             {:hidden @hidden?
+;;              :style #js {:width "96vw" :height "90vh"
+;;                          :border "1px solid #00000038"}}
+;;             [routes/selected-view]]]]]]]))
+;;   {:hidden? (reagent/atom false)}
+;;   {:frame false})
+
+
