2ddaf3ccd212259a934196ed9f154d990c8de5e8
João Paulo Soares <jpsoares106@gmail.com>
2020-04-08 18:10:42 -0300

    2020-04-07 2

--- src/nefroapp/overview_cards.cljs
+++ src/nefroapp/overview_cards.cljs
@@ -14,13 +14,18 @@
     [Paper :as material-paper]
     [MenuList :as material-menu-list]
     [MenuItem :as material-menu-item]
+    [tick.alpha.api :as tick]
+    [clojure.spec.alpha :as spec]
+    [cljs.test :refer-macros [is testing]]
+    [clojure.spec.gen.alpha :as gen]
+    [clojure.test.check.generators] ;; Is it necessary?
     ))
 
 ;; Draw the devcards ui.
 (devcards.core/start-devcard-ui!)
 
 (defcard
-  (str "# Telas do Nefro App!")
+  (str "# Oficina Nefro App!")
   {}
   {:frame false
    :heading false})
@@ -291,4 +296,77 @@
   (card-component
     [receita/component])
   {:hidden? (reagent/atom false)})
+
+(defn extract-string [today]
+  (fn [[last-value last-date & acc] date-value]
+    (let [{:keys [date value]} date-value
+          [_ year month day] (re-find #"(\d{4})\D(\d{1,2})\D(\d{1,2})" date)
+          [_ lyear lmonth lday] (re-find #"(\d{4})\D(\d{1,2})\D(\d{1,2})" last-date)
+          [_ current-year _ _] (re-find #"(\d{4})\D(\d{1,2})\D(\d{1,2})" today)
+          l-date (str day"/"month (when (not= current-year year) (str "/"year)))
+          current-date (str lday"/"lmonth (when (not= current-year lyear) (str "/"lyear)))
+          new-string (if last-value
+                        (str l-date"~"current-date" - "value)
+                        (str "Desde "l-date" - "value))]
+        (concat [value date] acc [new-string]))))
+
+(defn compact-history [today history]
+  (->> history
+       (partition-by :value)
+       (map last)
+       (reduce (extract-string today) [nil ""])
+       (drop 2)))
+
+;; ---------- SPECS for compact-history ----------
+  (def date-regex #"\d{4}-\d{2}-\d{2}")
+  (spec/def ::date (spec/and string? #(re-matches date-regex %)))
+  (spec/def ::value #{"100ml" "50ml" "200ml"})
+  (spec/def ::date-value (spec/keys :req-un [::date ::value]))
+  (spec/def ::history (spec/coll-of ::date-value :kind vector?))
+
+  (defn date-gen []
+    (gen/fmap #(str (tick/+ (tick/date "2019-01-01") (tick/new-period % :days)))
+              (gen/choose 0 730))) ;; 2 anos
+
+  (defn history-gen []
+    (gen/fmap #(->> % (sort-by :date) reverse vec)
+              (spec/gen ::history
+                        {::date date-gen})))
+
+  (spec/def ::today #{"2020-12-31"})
+  (spec/def ::history-arg
+    (spec/cat :today ::today
+              :history ::history))
+
+  (defn history-custom-gens []
+    (spec/gen ::history-arg {::history history-gen}))
+
+  (spec/fdef compact-history-with-custom-gens
+             :args (spec/with-gen
+                     ::history-arg
+                     history-custom-gens))
+
+  (comment
+    (gen/generate (spec/gen ::n))
+    (gen/sample (spec/gen ::n))
+
+    (gen/generate (gen/choose 0 730))
+    (gen/sample (date-gen))
+
+    (compact-history (gen/generate (history-custom-gens)))
+
+    (spec/exercise-fn `compact-history 1
+                      (spec/get-spec `compact-history-with-custom-gens))
+    )
+
+(deftest compact-history-example
+  (testing "Dado que o médico repetiu a receita em pelo menos 2 datas."
+    (testing "Quando mostrar histórico compactado."
+      (is (= '("Desde 29/12 - 200ml" "14/03~29/12 - 100ml" "15/12/2019~14/03 - 50ml" "12/04/2019~15/12/2019 - 100ml" "06/04/2019~12/04/2019 - 200ml")
+             (compact-history "2020-12-31" [ { :date "2020-12-29" :value "200ml" } { :date "2020-03-14" :value "100ml" } { :date "2020-01-26" :value "50ml" } { :date "2019-12-15" :value "50ml" } { :date "2019-04-12" :value "100ml" } { :date "2019-04-06" :value "200ml"}]))
+          "Então serão mostradas apenas as datas de quando o paciente começou e terminou aquela receita."))))
+
+(defcard exercise-fn-compact-history
+  (spec/exercise-fn `compact-history 1
+                    (spec/get-spec `compact-history-with-custom-gens)))
 
